4a5193aa5205570ab7c95670b3a6b6ca2a455159
Chandrima-Git <chandrimadutta98@gmail.com>
2020-10-27 21:33:14 +0530

    first user commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ { path: '', component: HomeComponent },
+  { path: 'homeLink', component: HomeComponent},
+  { path: 'loginLink', component: LoginComponent},
+  { path: 'registerLink', component:RegisterComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
